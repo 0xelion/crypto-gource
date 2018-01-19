18ade977421ff4a461d1a3bc28d290ba16b99f28
Xelion <mail@xelion.fr>
2018-01-19 20:20:42 +0100

    Add a cleanup function for Ethereum

---
+++ ethereum-project/cleanup.sh
@@ -0,0 +1,281 @@
+rm -rf yellowpaper
+rm -rf solidity
+rm -rf cpp-ethereum
+rm -rf tests
+rm -rf eth-bloom
+rm -rf py-evm
+rm -rf go-ethereum
+rm -rf ethereumj
+rm -rf eth-tester
+rm -rf web3.py
+rm -rf homestead-guide
+rm -rf remix
+rm -rf web3.js
+rm -rf plasma
+rm -rf mist
+rm -rf cbc-casper
+rm -rf evmjit
+rm -rf vyper
+rm -rf solc-bin
+rm -rf solidity-test-bytecode
+rm -rf eth-abi
+rm -rf wiki
+rm -rf ethereum-org
+#rm -rf hunter-cache
+rm -rf remixd
+rm -rf EIPs
+rm -rf browser-solidity
+rm -rf meteor-dapp-wallet
+rm -rf swarm-dapps
+rm -rf py-trie
+rm -rf sharding
+rm -rf eth-utils
+rm -rf eth-keys
+rm -rf research
+rm -rf pyethereum
+rm -rf py_pairing
+rm -rf casper
+rm -rf solc-js
+rm -rf remix-live
+rm -rf ethereum-org-website
+rm -rf populus
+rm -rf pyethapp
+rm -rf py-geth
+rm -rf pydevp2p
+rm -rf ethash
+rm -rf emacs-solidity
+rm -rf pyrlp
+rm -rf eth-keyfile
+rm -rf ens-registrar-dapp
+rm -rf dopple
+rm -rf pm
+rm -rf moon-browser
+rm -rf pyeth_keys
+rm -rf ens
+rm -rf homebrew-ethereum
+rm -rf py-solc
+rm -rf ecp
+rm -rf dapp-bin
+rm -rf py_ecc
+rm -rf documentation
+rm -rf common
+rm -rf solidity-examples
+rm -rf ropsten
+rm -rf serpent
+rm -rf meteor-dapp-catalog
+rm -rf moon-lang
+rm -rf btcrelay
+rm -rf blockies
+rm -rf langlab
+rm -rf meteor-package-accounts
+rm -rf moon-tool
+rm -rf meteor-package-tools
+rm -rf rpc-tests
+rm -rf benchmarking
+rm -rf dapp-styles
+rm -rf meteor-package-elements
+rm -rf stake-voice
+rm -rf cpp-dependencies
+#rm -rf go-casper
+rm -rf webthree-umbrella
+rm -rf ethereum-client-binaries
+rm -rf ethendance
+rm -rf simplecasper
+rm -rf oyente
+rm -rf diary
+rm -rf meteor-dapp-whisper-chat-client
+rm -rf ethereum-console
+rm -rf libsnark
+rm -rf scavenger_hunt
+rm -rf statesweep
+rm -rf meteor-ethereum-networkinfo
+rm -rf meteor-package-blocks
+rm -rf interfaces
+#rm -rf evmc
+rm -rf webthree-helpers
+rm -rf webthree
+rm -rf libweb3core
+rm -rf libethereum
+rm -rf alethzero
+rm -rf mix
+rm -rf economic-modeling
+rm -rf libwhisper
+rm -rf ethereum-buildbot
+rm -rf test-tools
+rm -rf vim-solidity
+rm -rf leveldb
+#rm -rf cpp-dependencies-win64
+rm -rf cryptopp
+rm -rf ethereum-dockers
+rm -rf ethereum-ppa
+#rm -rf jenkins-docker
+rm -rf guide
+rm -rf bench
+rm -rf devp2p
+rm -rf ethbot
+rm -rf node-ethereum
+rm -rf ethereum-docs
+rm -rf gethkey
+rm -rf cpp-ethereum-cmake
+rm -rf devcommon
+rm -rf genesis_block_generator
+rm -rf frontier-guide
+rm -rf pyethsaletool
+rm -rf solidity.js
+rm -rf ethereum.org
+rm -rf system-testing
+rm -rf jeff
+#rm -rf gav
+rm -rf go-build
+rm -rf natspec.js
+rm -rf profiling
+rm -rf cpp-ethash
+rm -rf ethdev-site
+rm -rf www
+rm -rf serpent-go
+rm -rf foundation
+rm -rf tryethereum
+rm -rf mining
+rm -rf news
+#rm -rf branding
+
+rm yellowpaper
+rm solidity
+rm cpp-ethereum
+rm tests
+rm eth-bloom
+rm py-evm
+rm go-ethereum
+rm ethereumj
+rm eth-tester
+rm web3.py
+rm homestead-guide
+rm remix
+rm web3.js
+rm plasma
+rm mist
+rm cbc-casper
+rm evmjit
+rm vyper
+rm solc-bin
+rm solidity-test-bytecode
+rm eth-abi
+rm wiki
+rm ethereum-org
+#rm hunter-cache
+rm remixd
+rm EIPs
+rm browser-solidity
+rm meteor-dapp-wallet
+rm swarm-dapps
+rm py-trie
+rm sharding
+rm eth-utils
+rm eth-keys
+rm research
+rm pyethereum
+rm py_pairing
+rm casper
+rm solc-js
+rm remix-live
+rm ethereum-org-website
+rm populus
+rm pyethapp
+rm py-geth
+rm pydevp2p
+rm ethash
+rm emacs-solidity
+rm pyrlp.txt
+rm eth-keyfile.txt
+rm ens-registrar-dapp.txt
+rm dopple.txt
+rm pm.txt
+rm moon-browser.txt
+rm pyeth_keys.txt
+rm ens.txt
+rm homebrew-ethereum.txt
+rm py-solc.txt
+rm ecp.txt
+rm dapp-bin.txt
+rm py_ecc.txt
+rm documentation.txt
+rm common.txt
+rm solidity-examples.txt
+rm ropsten.txt
+rm serpent.txt
+rm meteor-dapp-catalog.txt
+rm moon-lang.txt
+rm btcrelay.txt
+rm blockies.txt
+rm langlab.txt
+rm meteor-package-accounts.txt
+rm moon-tool.txt
+rm meteor-package-tools.txt
+rm rpc-tests.txt
+rm benchmarking.txt
+rm dapp-styles.txt
+rm meteor-package-elements.txt
+rm stake-voice.txt
+rm cpp-dependencies.txt
+#rm go-casper.txt
+rm webthree-umbrella
+rm ethereum-client-binaries.txt
+rm ethendance.txt
+rm simplecasper.txt
+rm oyente.txt
+rm diary.txt
+rm meteor-dapp-whisper-chat-client.txt
+rm ethereum-console.txt
+rm libsnark.txt
+rm scavenger_hunt.txt
+rm statesweep.txt
+rm meteor-ethereum-networkinfo.txt
+rm meteor-package-blocks.txt
+rm interfaces.txt
+#rm evmc.txt
+rm webthree-helpers.txt
+rm webthree.txt
+rm libweb3core.txt
+rm libethereum.txt
+rm alethzero.txt
+rm mix.txt
+rm economic-modeling.txt
+rm libwhisper.txt
+rm ethereum-buildbot.txt
+rm test-tools.txt
+rm vim-solidity.txt
+rm leveldb.txt
+#rm cpp-dependencies-win64.txt
+rm cryptopp.txt
+rm ethereum-dockers.txt
+rm ethereum-ppa.txt
+#rm jenkins-docker.txt
+rm guide.txt
+rm bench.txt
+rm devp2p.txt
+rm ethbot.txt
+rm node-ethereum.txt
+rm ethereum-docs.txt
+rm gethkey.txt
+rm cpp-ethereum-cmake.txt
+rm devcommon.txt
+rm genesis_block_generator.txt
+rm frontier-guide.txt
+rm pyethsaletool.txt
+rm solidity.js.txt
+rm ethereum.org.txt
+rm system-testing.txt
+rm jeff.txt
+#rm gav.txt
+rm go-build.txt
+rm natspec.js.txt
+rm profiling.txt
+rm cpp-ethash.txt
+rm ethdev-site.txt
+rm www.txt
+rm serpent-go.txt
+rm foundation.txt
+rm tryethereum.txt
+rm mining.txt
+rm news.txt
+#rm branding.txt
