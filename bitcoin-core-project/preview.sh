fa0081e25afc97f702b9c7d801fa95ec1581da6c
Xelion <mail@xelion.fr>
2018-01-20 13:48:05 +0100

    Fixing the titles

--- bitcoin-core-project/preview.sh
+++ bitcoin-core-project/preview.sh
@@ -1,1 +1,1 @@
-gource -1920x1080 -r 60 --seconds-per-day 0.2 --file-idle-time 0 --key --title 'Bitcoin Core Project' --caption-file caption.txt --caption-size 24 --caption-duration 5 --user-image-dir avatar/ bitcoin-core-project.txt
+gource -1920x1080 -r 60 --seconds-per-day 0.2 --file-idle-time 0 --key --title "Bitcoin Core Project" --caption-file caption.txt --caption-size 24 --caption-duration 5 --user-image-dir avatar/ bitcoin-core-project.txt
