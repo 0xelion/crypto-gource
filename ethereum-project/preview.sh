c547d1217cfddc1ca544d3d600b765429eacdc1c
Xelion <mail@xelion.fr>
2018-01-20 20:38:46 +0100

    Speeding up the Ethereum preview/render

--- ethereum-project/preview.sh
+++ ethereum-project/preview.sh
@@ -1,1 +1,1 @@
-gource -1920x1080 -r 60 --seconds-per-day 0.4 --file-idle-time 0 --key --title "Ethereum Project" --caption-file caption.txt --caption-size 24 --caption-duration 5 --user-image-dir avatar/ ethereum-project.txt
+gource -1920x1080 -r 60 --seconds-per-day 0.3 --file-idle-time 0 --key --title "Ethereum Project" --caption-file caption.txt --caption-size 24 --caption-duration 5 --user-image-dir avatar/ ethereum-project.txt
