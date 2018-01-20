387fcb68db3358b2f71a58b7afd11e7da7e1d767
Xelion <mail@xelion.fr>
2018-01-20 21:21:39 +0100

    Creating IOTA project cleanup script

--- iota-project/cleanup.sh
+++ iota-project/cleanup.sh
@@ -1,3 +1,75 @@
 rm -rf iri
+rm -rf wallet
+rm -rf iota.lib.java
+rm -rf cli-app
+rm -rf iota.lib.py
+rm -rf iota.flash.js
+rm -rf curl.lib.js
+rm -rf iota.lib.go
+rm -rf ccurl.interface.js
+rm -rf iota.lib.js
+rm -rf Snapshot.ixi
+rm -rf kerl
+rm -rf android-wallet-app
+rm -rf iota.lib.csharp
+rm -rf wiki
+rm -rf iota.crypto.js
+rm -rf sandbox
+rm -rf libopencl-stub
+rm -rf ccurl
+rm -rf iota-bindings
+rm -rf iota.rs
+rm -rf MAM
+rm -rf mam.client.js
+rm -rf documentation
+rm -rf crypto.iota.js
+rm -rf iota.rs.js
+rm -rf MAM.ixi
+rm -rf giotan
+rm -rf iri-snapcraft
+rm -rf light-wallet
+rm -rf stresstest
+rm -rf bug-bounty
+rm -rf PearlDiver
+rm -rf clcontext
+rm -rf wallet-packager
+rm -rf iota.curl.java
+rm -rf iota-gui-beta
 
 rm iri.txt
+rm wallet.txt
+rm iota.lib.java.txt
+rm cli-app.txt
+rm iota.lib.py.txt
+rm iota.flash.js.txt
+rm curl.lib.js.txt
+rm iota.lib.go.txt
+rm ccurl.interface.js.txt
+rm iota.lib.js.txt
+rm Snapshot.ixi.txt
+rm kerl.txt
+rm android-wallet-app.txt
+rm iota.lib.csharp.txt
+rm wiki.txt
+rm iota.crypto.js.txt
+rm sandbox.txt
+rm libopencl-stub.txt
+rm ccurl.txt
+rm iota-bindings.txt
+rm iota.rs.txt
+rm MAM.txt
+rm mam.client.js.txt
+rm documentation.txt
+rm crypto.iota.js.txt
+rm iota.rs.js.txt
+rm MAM.ixi.txt
+rm giotan.txt
+rm iri-snapcraft.txt
+rm light-wallet.txt
+rm stresstest.txt
+rm bug-bounty.txt
+rm PearlDiver.txt
+rm clcontext.txt
+rm wallet-packager.txt
+rm iota.curl.java.txt
+rm iota-gui-beta.txt
