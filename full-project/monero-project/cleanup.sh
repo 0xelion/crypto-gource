1589c83ddd0b5d8c0cd0ade7ee10d6d0aa390ef4
Xelion <mail@xelion.fr>
2018-01-21 16:01:45 +0100

    Fix the cleanup Monero script

--- full-project/monero-project/cleanup.sh
+++ full-project/monero-project/cleanup.sh
@@ -1,9 +1,33 @@
-rm -rf bitcoin
-rm -rf bips
-rm -rf libblkmaker
-rm -rf libbase58
+rm -rf monero
+rm -rf monero-gui
+rm -rf kovri
+rm -rf kovri-site
+rm -rf meta
+rm -rf monero-site
+rm -rf sekura
+rm -rf research-lab
+rm -rf kovri-docs
+rm -rf supercop
+rm -rf miniupnpc
+rm -rf monero-forum
+rm -rf unbound
+rm -rf xmr-seeder
+rm -rf urs
+rm -rf mininero
 
-rm bitcoin.txt
-rm bips.txt
-rm libblkmaker.txt
-rm libbase58.txt
+rm monero.txt
+rm monero-gui.txt
+rm kovri.txt
+rm kovri-site.txt
+rm meta.txt
+rm monero-site.txt
+rm sekura.txt
+rm research-lab.txt
+rm kovri-docs.txt
+rm supercop.txt
+rm miniupnpc.txt
+rm monero-forum.txt
+rm unbound.txt
+rm xmr-seeder.txt
+rm urs.txt
+rm mininero.txt
